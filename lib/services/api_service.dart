154281ea96a928b34b3e0b32f8b42f4051b8ca5c
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2023-02-01 22:36:15 +0900

    add model, rest api

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,13 +1,21 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:webtoon_app_2023/models/webtoon_model.dart';
 
 class ApiServce {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = 'today';
-  void getTodaysToons() async {
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = 'today';
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
+    List<WebtoonModel> webtoonInstances = [];
     if (response.statusCode == 200) {
-      print(response.body);
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
     }
+    return webtoonInstances;
   }
 }
